6168e4899dfcfbc1531f5c5a0f2e3743708028c7
Arjen de Blok <arjen@deblok.org>
2018-11-05 07:15:04 +0100

    Added MDC components

---
+++ src/mdc/index.ts
@@ -0,0 +1,8 @@
+import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+
+export function configure(config: FrameworkConfiguration) {
+  config.globalResources([
+    PLATFORM.moduleName("./mdc-ripple"),
+    PLATFORM.moduleName("./text-field/mdc-text-field")
+  ]);
+}
---
+++ src/mdc/text-field/mdc-text-field.ts
@@ -0,0 +1,67 @@
+import { bindable, autoinject } from 'aurelia-framework';
+import { MDCTextField } from '@material/textfield/dist/mdc.textfield';
+import './mdc-text-field.scss';
+
+@autoinject()
+export class MdcTextField {
+    textField: MDCTextField;
+    @bindable id: string;
+    @bindable label: string; // translate via t="[label]translatekey"
+    @bindable model: object;
+    @bindable property: string = "value";
+    @bindable type: string = "text";
+    @bindable required: boolean = true;
+    @bindable disabled: boolean = false;
+    @bindable helperText: string;
+    @bindable prefilled: boolean = false;
+    @bindable autofocus: boolean = false;
+    @bindable autocomplete: string = "on";
+    valErrors: string[] = [];
+
+    constructor(private element: Element) {
+    }
+
+    public created() {
+        var isIE11 = (navigator.userAgent.indexOf("Trident") > 0);
+        if(this.type=="date" && isIE11) this.type = "text";
+    }
+
+    bind(context) {
+        if(this.id === undefined) {
+            console.error("id not defined");
+        }
+
+        if(!this.model) {
+            this.model = context;
+        }
+
+        const inputElement = this.element.querySelector("input");
+        if (inputElement && this.required) {
+            inputElement.setAttribute("required", "true");
+        }
+        if (inputElement && this.disabled) {
+            inputElement.setAttribute("disabled", "disabled");
+        }
+
+        const textFieldElement = this.element.querySelector(".mdc-text-field");
+        if (textFieldElement && this.disabled) {
+            textFieldElement.classList.add("mdc-text-field--disabled");        
+        }
+    }
+
+    attached() {
+        const textFieldElement = this.element.querySelector(".mdc-text-field");
+        this.textField = new MDCTextField(textFieldElement);
+
+        // override isValid
+        const foundation = this.textField.foundation_;
+        var self = this;
+        foundation.isValid = () : boolean => {
+            return self.valErrors.length == 0;
+        };
+    }
+
+    detached() {
+        this.textField.destroy();
+    }
+}
