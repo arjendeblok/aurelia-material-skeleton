8b46076b9794323a90f982a5d3510854943b7e91
Arjen de Blok <arjen@deblok.org>
2018-11-11 16:14:57 +0100

    Added select to demo.

--- src/demo/demo-model.ts
+++ src/demo/demo-model.ts
@@ -3,6 +3,7 @@ export class DemoModel {
     email: string = "";
     numeric: number = 1;
     checked: boolean = false;
+    selected: string = "";
 
     get percentage() : number {
         return this.numeric / 100;
