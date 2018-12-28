70f40e1526ac1c00f5a201301103feb8f3d95c0b
Arjen de Blok <arjen@deblok.org>
2018-12-28 16:56:14 +0100

    Added validation checking.

--- src/mdc/switch/README.md
+++ src/mdc/switch/README.md
@@ -1,8 +1,25 @@
 
 # Material Design Switch component
 
+## Example
+
+```html
+<mdc-switch 
+    id="switch-input" 
+    label="Switch me" 
+    checked.bind="checked & validate" 
+    required="true" 
+    mdc-validation-errors>
+</mdc-switch>
+```
+
 ## Attributes
 
 1. `id` is the identifier of the element and must have an unique value (**required**);
-2. `label` holds the label
-3. `checked` is the boolean value of the switch
+2. `label` holds the label;
+3. `checked` is the boolean value of the switch;
+4. `required` indicates if the input is a required field; is independent of the Aurelia validation required(), so both must be set.
+
+## Important
+
+You need to add the `mdc-validation-errors` custom attribute and an `validate` value behavior on the `value` attribute to make the validation work.
--- src/mdc/switch/mdc-switch.ts
+++ src/mdc/switch/mdc-switch.ts
@@ -7,19 +7,26 @@ import './mdc-switch.scss';
 export class MdcSwitch {
     private switch: MDCSwitch;
     @bindable id: string;
-    @bindable label: string;
+    @bindable label: string
+    @bindable required: boolean|string;
     @bindable({ defaultBindingMode: bindingMode.fromView }) checked: boolean = false;
+    @bindable validationErrors: any[] = [];
 
     constructor(private element: Element) {
+    }
+
+    bind() {
+        if(this.required === "true" || this.required === "required") {
+            this.required = true;
+        }
     }
 
     attached() {
         const element = this.element.querySelector(".mdc-switch");
         this.switch = new MDCSwitch(element);
+    }
 
-        const input = this.element.querySelector("input");
-        input.onblur = (ev:FocusEvent) => {
-            this.element.dispatchEvent(new FocusEvent("blur"));
-        };
+    get valid() {
+        return this.validationErrors.length == 0;
     }
 }
